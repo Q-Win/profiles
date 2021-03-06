0f991e8c550a46c598fdee1955f1d017583f2d45
Quinn <quinn.krug@gmail.com>
2021-03-06 14:17:54 -0800

    add auth and permissions to viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 from profiles_api import serializers
 from profiles_api import models
@@ -101,3 +103,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
